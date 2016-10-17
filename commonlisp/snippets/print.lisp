1a8f55fda34888a7af34fdaf9035a228ea26accb
Avinash Tiwari <avitiw@gmail.com>
2016-10-17 15:57:20 +0530

    Formatting + ignore emacs temp files

--- commonlisp/snippets/print.lisp
+++ commonlisp/snippets/print.lisp
@@ -3,10 +3,11 @@
      for i from 1 to n
      do
        (loop for j from 1 to (- n i)
-	    do (format t "-"))
+	  do (format t "-"))
        (loop for j from 1 to (- (* 2 i) 1)
 	  do (prin1 '*))
-              (loop for j from 1 to (- n i)
-	    do (format t "-"))
+       (loop for j from 1 to (- n i)
+	  do (format t "-"))
        (format t "~%")))
-(printTriangle 23)
+
+;(printTriangle 23)
